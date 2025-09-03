57dd11e744971213b40c3a18f52e23a2aae2d859
siddharth-1407 <sanksiddharth@gmail.com>
2025-09-03 17:34:36 +0530

    Add getStructuredOutputPath utility function to determine output paths based on OS. Update conversion services to utilize the new function.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,6 @@
 import fs from 'fs';
+import os from 'os';
+import path from 'path';
 
 export function cleanupFiles(paths: string[]) {
 	for (const file of paths) {
@@ -6,4 +8,27 @@ export function cleanupFiles(paths: string[]) {
 			fs.unlinkSync(file);
 		}
 	}
+}
+
+export function getStructuredOutputPath(): string {
+	let structuredOutputPath: string;
+
+	switch (os.platform()) {
+		case 'win32':
+			structuredOutputPath = path.join(process.cwd(), 'src', 'lib', 'apryse', 'StructuredOutputWindows', 'Lib', 'Windows');
+			break;
+		case 'darwin':
+			structuredOutputPath = path.join(process.cwd(), 'src', 'lib', 'apryse', 'StructuredOutputMac', 'Lib', 'MacOS');
+			break;
+		case 'linux':
+			if (os.arch() === 'arm64') {
+				structuredOutputPath = path.join(process.cwd(), 'src', 'lib', 'apryse', 'StructuredOutputLinuxArm64', 'Lib', 'Linux');
+			} else {
+				structuredOutputPath = path.join(process.cwd(), 'src', 'lib', 'apryse', 'StructuredOutputLinux', 'Lib', 'Linux');
+			}
+			break;
+		default:
+			throw new Error(`Unsupported OS: ${os.platform()}`);
+	}
+	return structuredOutputPath;
 }
--- src/services/conversion-services.ts
+++ src/services/conversion-services.ts
@@ -1,34 +1,10 @@
-import os from 'os';
 import fs from 'fs';
 import path from 'path';
 import { v4 as uuidv4 } from 'uuid';
 import pkg from '@pdftron/pdfnet-node';
-import { cleanupFiles } from '../lib/utils';
+import { cleanupFiles, getStructuredOutputPath } from '../lib/utils';
 
 const { PDFNet } = pkg;
-
-function getStructuredOutputPath(): string {
-	let structuredOutputPath: string;
-
-	switch (os.platform()) {
-		case 'win32':
-			structuredOutputPath = path.join(process.cwd(), 'src', 'lib', 'apryse', 'StructuredOutputWindows', 'Lib', 'Windows');
-			break;
-		case 'darwin':
-			structuredOutputPath = path.join(process.cwd(), 'src', 'lib', 'apryse', 'StructuredOutputMac', 'Lib', 'MacOS');
-			break;
-		case 'linux':
-			if (os.arch() === 'arm64') {
-				structuredOutputPath = path.join(process.cwd(), 'src', 'lib', 'apryse', 'StructuredOutputLinuxArm64', 'Lib', 'Linux');
-			} else {
-				structuredOutputPath = path.join(process.cwd(), 'src', 'lib', 'apryse', 'StructuredOutputLinux', 'Lib', 'Linux');
-			}
-			break;
-		default:
-			throw new Error(`Unsupported OS: ${os.platform()}`);
-	}
-	return structuredOutputPath;
-}
 
 export async function pdfToPptxService(pdfBuffer: Buffer): Promise<Buffer> {
 	const inputPath = path.join(process.cwd(), 'temp', `${uuidv4()}.pdf`);
